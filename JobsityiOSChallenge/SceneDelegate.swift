62236d9cf2c448c12a6e4538a659d11fa47ca16c
AntonioCarlosCNJ <antonio.correa0407@gmail.com>
2022-03-02 18:21:48 -0300

    Adding the Xcode Project

---
+++ JobsityiOSChallenge/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  JobsityiOSChallenge
+//
+//  Created by Antonio Carlos on 02/03/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let vc = UIViewController()
+        vc.view.backgroundColor = .blue
+        window.rootViewController = vc
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,16 @@
+target 'JobsityiOSChallenge' do
+  use_frameworks!
+
+  # Pods for JobsityiOSChallenge
+  pod 'SnapKit', '~> 5.0.0'
+
+  target 'JobsityiOSChallengeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'JobsityiOSChallengeUITests' do
+    # Pods for testing
+  end
+
+end
